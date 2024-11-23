d3e38d3d277e4bb79b10e68397d35d9d68e5d866
irana4 <irana4@uwo.ca>
2024-11-23 01:08:50 -0500

    update task 6

--- SQL/ex6.sql
+++ SQL/ex6.sql
@@ -2,22 +2,33 @@
 
 -- 1. INSERT Command
 -- Add new data to the UserWatchlist table by selecting users and movies.
-INSERT INTO UserWatchlist (userId, movieId)
-SELECT userId, movieId
+INSERT INTO UserWatchlist (username, movieTitle, releaseDate, watchlistUpdateDate)
+SELECT username, title, releaseDate, CURDATE()
 FROM User, Movie
-WHERE Movie.genre = 'Action';
+WHERE Movie.genre = 'Action'
+AND NOT EXISTS (
+    SELECT 1
+    FROM UserWatchlist
+    WHERE UserWatchlist.username = User.username
+    AND UserWatchlist.movieTitle = Movie.title
+    AND UserWatchlist.releaseDate = Movie.releaseDate
+);
+
 
 -- 2. UPDATE Command
--- Increase the ratings of all comedy movies with low ratings.
 UPDATE Movie
-SET rating = rating + 0.5
-WHERE genre = 'Comedy' AND rating < 8.0;
+SET duration = duration + 10
+WHERE genre = 'Comedy'
+LIMIT 10;
+
 
 -- 3. DELETE Command
 -- Remove likes by inactive users from the ReviewLikes table.
 DELETE FROM ReviewLikes
-WHERE userId IN (
-    SELECT userId
+WHERE username IN (
+    SELECT username
     FROM User
-    WHERE accountStatus = 'Inactive'
-);
+    WHERE watchlistUpdateDate IS NULL
+)
+LIMIT 10;
+
